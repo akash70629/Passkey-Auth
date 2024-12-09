48c07e83f97caecf4b364e124a08f27603f079e1
akash70629 <akash70629@gmail.com>
2024-12-09 23:28:56 +0530

    .

--- index.js
+++ index.js
@@ -58,9 +58,7 @@ app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
     console.log(`Server running on port ${PORT}`);
     console.log(`Server running on port ${PORT}`);
-    console.log(`Server running on port ${PORT}`);
-    console.log(`Server running on port ${PORT}`);
-    console.log(`Server running on port ${PORT}`);
+    
     console.log(`Server running on port ${PORT}`);
     console.log(`Server running on port ${PORT}`);
     console.log(`Server running on port ${PORT}`);
